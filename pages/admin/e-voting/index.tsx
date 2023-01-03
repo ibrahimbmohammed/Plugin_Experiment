19dc1598dad55d6f2adf3884cba70b90655ed5ff
Ibrahim <ibrahimbmohammed@gmail.com>
2023-01-03 01:26:03 +0100

    more hello color correction

--- pages/admin/e-voting/index.tsx
+++ pages/admin/e-voting/index.tsx
@@ -4,8 +4,8 @@ function TemplateOne() {
   const [recievedMessage, setRecievedMessage] = useState("");
   return (
     <div className="flex flex-col space-y-10">
-      <p className="text-lg h-full w-full bg-amber-700-600">
-        Template donations:{recievedMessage}
+      <p className="text-lg h-full w-full bg-amber-700">
+        Template evoting:{recievedMessage}
       </p>{" "}
     </div>
   );
--- pages/admin/trainings/index.tsx
+++ pages/admin/trainings/index.tsx
@@ -4,7 +4,7 @@ function TemplateOne() {
   const [recievedMessage, setRecievedMessage] = useState("");
   return (
     <div className="flex flex-col space-y-10">
-      <p className="text-lg h-full w-full bg-fuchsia-500-500">
+      <p className="text-lg h-full w-full bg-fuchsia-500">
         Template training:{recievedMessage}
       </p>{" "}
     </div>
